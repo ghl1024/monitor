864c81a282f4f801edd3def9d08954c9df3c8115
cloud-op <jiaozhenqing@gmail.com>
2020-05-09 19:00:51 +0800

    Create iam-monitor.sh

---
+++ console/iam-monitor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#定义的是命令执行的超时时间
+readonly TIMESEC="20"
+#将输出结果默认赋值
+result="-1"
+
+#检查输出文件的目录，文件和权限
+function check_prometheus
+{
+    mkdir -p  /var/lib/node_exporter/textfile
+    cd /var/lib/node_exporter/textfile && touch iam_monitor.prom && chmod 755 iam_monitor.prom
+}
+
+#通过云存储的接口获取指定文件的内容并和预先定义的内容进行比对
+function check_result
+{
+    cd /monitor/terraform
+    local start=$(date +%s%N)
+    result=$( timeout $TIMESEC terraform apply -auto-approve 2>&1|grep -c BucketAlreadyExists)
+    local end=$(date +%s%N)
+    local cost=$[$end-$start]
+
+    if [ "$result" -eq 1 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 0\niam_read_cost $cost" > iam_monitor.prom
+    else
+        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 1\niam_read_cost $cost" > iam_monitor.prom
+    fi
+}
+
+function main
+{
+    check_prometheus
+    check_result
+}
+
+main
