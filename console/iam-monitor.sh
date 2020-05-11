8c210f36d36205a085c5084ca5252c67c78f5729
cloud-op <jiaozhenqing@gmail.com>
2020-05-11 14:26:54 +0800

    Update iam-monitor.sh

--- console/iam-monitor.sh
+++ console/iam-monitor.sh
@@ -1,29 +1,65 @@
 #!/bin/bash
 #定义的是命令执行的超时时间
-readonly TIMESEC="20"
+readonly TIMESEC="30"
 #将输出结果默认赋值
 result="-1"
-
+count=0
+s3_mess="BucketAlreadyExists"
+rds_mess="Monitor1234"
+vpc_mess="CONFLICT"
+instance_mess="PrimaryIpAddress"
 #检查输出文件的目录，文件和权限
 function check_prometheus
 {
     mkdir -p  /var/lib/node_exporter/textfile
-    cd /var/lib/node_exporter/textfile && touch iam_monitor.prom && chmod 755 iam_monitor.prom
+    cd /var/lib/node_exporter/textfile && touch console_monitor.prom && chmod 755 console_monitor.prom
 }
 
-#通过terraform创建一个S3的bucket，来间接验证IAM的可用性，当然会受到S3可用性的影响
+#通过云存储的接口获取指定文件的内容并和预先定义的内容进行比对
 function check_result
 {
     cd /monitor/terraform
-    local start=$(date +%s)
-    result=$( timeout $TIMESEC terraform apply -auto-approve 2>&1|grep -c BucketAlreadyExists)
-    local end=$(date +%s)
+    local start=$(date +%s%N)
+    result=$( timeout $TIMESEC terraform apply -auto-approve 2>&1)
+    result_s3=$(echo $result|grep -c $s3_mess)
+    result_rds=$(echo $result|grep -c $rds_mess)
+    result_vpc=$(echo $result|grep -c $vpc_mess)
+    result_instance=$(echo $result|grep -c $instance_mess)
+    local end=$(date +%s%N)
     local cost=$[$end-$start]
 
-    if [ "$result" -eq 1 ];then
-        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 0\niam_read_cost $cost" > iam_monitor.prom
+    if [ "$result_s3" -eq 1 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_s3_monitor_status 0" > console_monitor.prom
+        count=$((count + 1 ))
     else
-        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 1\niam_read_cost $cost" > iam_monitor.prom
+        cd /var/lib/node_exporter/textfile && echo -e "iam_s3_monitor_status 1" > console_monitor.prom
+    fi
+
+    if [ "$result_rds" -eq 1 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_rds_monitor_status 0" >> console_monitor.prom
+        count=$((count + 1 ))
+    else
+        cd /var/lib/node_exporter/textfile && echo -e "iam_rds_monitor_status 1" >> console_monitor.prom
+    fi
+
+    if [ "$result_vpc" -eq 1 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_vpc_monitor_status 0" >> console_monitor.prom
+        count=$((count + 1 ))
+    else
+        cd /var/lib/node_exporter/textfile && echo -e "iam_vpc_monitor_status 1" >> console_monitor.prom
+    fi
+
+    if [ "$result_instance" -eq 1 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_instance_monitor_status 0" >> console_monitor.prom
+        count=$((count + 1 ))
+    else
+        cd /var/lib/node_exporter/textfile && echo -e "iam_instance_monitor_status 1" >> console_monitor.prom
+    fi
+
+    if [ "$count" -gt 2 ];then
+        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 0\niam_read_cost $cost" >> console_monitor.prom
+    else
+        cd /var/lib/node_exporter/textfile && echo -e "iam_monitor_status 1\niam_read_cost $cost" >> console_monitor.prom
     fi
 }
 
