03546ac033a451c36e0342c3c10e97787c99adb3
zhangli-hub <60000564+zhangli-hub@users.noreply.github.com>
2021-04-09 16:47:31 +0800

    Update redis-monitor.sh

--- redis/redis-monitor.sh
+++ redis/redis-monitor.sh
@@ -11,7 +11,7 @@ set -x
 #默认情况下，仅需要修改SERVER、PORT、PASSWORD的值，即可执行脚本进行对redis服务的可用性监控
 
 readonly SERVER="redis-8uyj8b5ymwt1-proxy-nlb.jvessel-open-sh.jdcloud.com"
-readonly PASSWORD="1111111111"
+readonly PASSWORD="XXXXXXXXXXXXXXX"
 readonly PORT="6379"
 
 #key的定义要尽量复杂，避免和业务的key冲突了，所以增加了一个random的值
